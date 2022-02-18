3fe95bbebe0d0592cd4e06f05e40f8240ac4464e
Muhammad Azim <azeemamjad412@gmail.com>
2022-02-17 19:03:50 -0700

    Initially Added sucessfully

--- Week1/ArrayIndexDeletion.c
+++ Week1/ArrayIndexDeletion.c
@@ -3,32 +3,23 @@
 
 int main(void)
 {
-    int desiredNo;
     int desiredIndex;
-    int temp;
-    int temp1;
-
-    int arr[SIZE] = {0,1,2,3,4,5,6,7,8};
+    int arr[SIZE] = {0,1,2,3,4,5,6,7,8,9};
 
     for (int i = 0; i < SIZE; i++)
     {
         printf("Elements before insertion at index %d: %d\n", i,arr[i]);
     }
 
-    printf("\nEnter the number you want to insert: ");
-    scanf("%d", &desiredNo);
-
     printf("\nEnter the index to insert there: ");
     scanf("%d", &desiredIndex);
 
-    for (int i = (SIZE- 1); i > desiredIndex; i--)
+    for (int i = desiredIndex; i < SIZE; i++)
     {
-        arr[i]= arr[i-1];
+        arr[i]= arr[i+1];
     }
 
-    arr[desiredIndex] = desiredNo;
-
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < (SIZE-1); i++)
     {
         printf("Element after insertion at index %d: %d\n", i,arr[i]);
     }
